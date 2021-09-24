f182cc8b4ffb190c4efeda739770fda82ee59f4c
AlejandroVillalba <alejandrovillalba007@gmail.com>
2021-09-24 17:14:11 -0400

    Componente Livewire

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
     <div class="text-center">
-        <h1 class="text-2xl text-gray-700 mb-2 uppercase">Últimos cursos</h1>
+        <h1 class="mb-2 text-3xl text-gray-700 uppercase">Últimos cursos</h1>
         <h2 class="text-xl text-gray-600">Fórmate online como profesional en tecnologia</h2>
         <h3 class="text-lg text-gray-600">70% de los egresados duplican sus ingresos</h3>
     </div>
+
+    <livewire:course-list>
 @endsection
