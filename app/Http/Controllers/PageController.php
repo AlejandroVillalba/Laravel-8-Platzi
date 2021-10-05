82c01b818df4b30e7217fddd10d272174236e316
Alejandro Villalba <alejandrovillalba007@gmail.com>
2021-10-05 00:20:32 -0300

    Panel Administrativo

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class PageController extends Controller
 {
     public function home(){
 
-        return view('home');
+        return view('index');
     }
 
-    public function course(){
+    public function course(Course $course){
 
-        return view('course');
+        return view('course', compact('course'));
     }
 }
